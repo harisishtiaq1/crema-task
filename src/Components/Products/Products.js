a0112cdf57603ea795606fbd549a2338cee46197
harisishtiaq1 <harisharry232@gmail.com>
2023-04-13 15:54:40 +0500

    Updated

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -4,23 +4,21 @@ import React from "react";
 
 function Products() {
   return (
-    <Box>
-      <Paper sx={{ backgroundColor: "blueviolet", height: "200px" }}>
-        <Box
-          sx={{
-            backgroundColor: "white",
-            width: "200px",
-            borderRadius: "10px",
-            padding: "1px",
-          }}
-        >
-          <Stack direction="row">
-            <InputBase sx={{ ml: 3, mt: 0.5 }} placeholder="Search here" />
-            <IconButton>
-              <SearchIcon />
-            </IconButton>
-          </Stack>
-        </Box>
+    <Box sx={{ backgroundColor: "blueviolet", height: "200px" }}>
+      <Paper
+        sx={{
+          backgroundColor: "white",
+          width: "200px",
+          borderRadius: "10px",
+          padding: "1px",
+        }}
+      >
+        <Stack direction="row">
+          <InputBase sx={{ ml: 3, mt: 0.5 }} placeholder="Search here" />
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+        </Stack>
       </Paper>
     </Box>
   );
