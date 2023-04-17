a6c553a6173eb5cad47dab3393e667c4873aeaea
harisishtiaq1 <harisharry232@gmail.com>
2023-04-17 10:11:19 +0500

    Updated

--- src/Components/RecentOrders/Orders.js
+++ src/Components/RecentOrders/Orders.js
@@ -17,31 +17,154 @@ import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-function createData(id, calories, fat, carbs, protein) {
-  return { id, calories, fat, carbs, protein };
+import Scrollbars from "react-custom-scrollbars";
+
+const thumbStyle = {
+  backgroundColor: "#888",
+  borderRadius: "6px",
+  cursor: "pointer",
+  width: "4px",
+  height: "5px",
+};
+function createData(
+  id,
+  product,
+  customer,
+  delivery,
+  price,
+  payment,
+  status,
+  action
+) {
+  return { id, product, customer, delivery, price, payment, status, action };
 }
 
 const rows = [
-  createData("#SK231", "HP Pavilion Gaming Ryzen 7 Octa Core 4800H", "Ina Hughes", 24, 4.0),
-  createData("#SK232", "Addida Shoes", "Myrtie Ferguson", 37, 4.3),
-  createData("#SK233", "Sleeve Jacket", "Johnny Herrera", 24, 6.0),
-  createData("#SK234", "Mens's Exclusive Watch", "Ina Hughes", 67, 4.3),
-  createData("#SK235", "Addida Shoes", "Myrtie Ferguson", 49, 3.9),
-  createData("#SK236", "Sleeve Jacket", "Johnny Herrera", 24, 4.0),
-  createData("#SK237", "Bicycle", "Ina Hughes", "Myrtie Ferguson", 4.3),
-  createData("#SK238", "Addida Shoes", "Johnny Herrera", 24, 6.0),
-  createData("#SK239", "Sleeve Jacket", "Myrtie Ferguson", 67, 4.3),
-  createData("#SK240", "Bicycle", "Ina Hughes", 49, 3.9),
-  createData("#SK241", "Addida Shoes", "Myrtie Ferguson", 24, 4.0),
-  createData("#SK242", "Sleeve Jacket", "Johnny Herrera", 37, 4.3),
-  createData("#SK243", "Bicycle", "Ina Hughes", 24, 6.0),
-  createData("#SK244", "Addida Shoes", "Myrtie Ferguson", 67, 4.3),
-  createData("#SK245", "Sleeve Jacket", "Johnny Herrera"),
-  createData("#SK246", "Bicycle", "Ina Hughes", "Johnny Herrera", 4.0),
-  createData("#SK247", "Addida Shoes", "Myrtie Ferguson", 37, 4.3),
-  createData("#SK248", "Sleeve Jacket","Johnny Herrera", 6.0),
-  createData("#SK249", "Bicycle", "Ina Hughes", 67, 4.3),
-  createData("#SK250", "Addida Shoes", "Myrtie Ferguson", 49, 3.9),
+  createData(
+    "#SK231",
+    "HP Pavilion Gaming Ryzen 7 Octa Core 4800H",
+    "Ina Hughes",
+    "08-21-2020",
+    "$1250",
+    "COD"
+  ),
+  createData(
+    "#SK232",
+    "Addida Shoes",
+    "Myrtie Ferguson",
+    "08-12-2020",
+    "$100",
+    "Prepaid"
+  ),
+  createData(
+    "#SK233",
+    "Sleeve Jacket",
+    "Johnny Herrera",
+    "07-30-2020",
+    "$1020",
+    "Prepaid"
+  ),
+  createData(
+    "#SK234",
+    "Mens's Exclusive Watch",
+    "Ina Hughes",
+    "08-21-2020",
+    "$1250",
+    "COD"
+  ),
+  createData(
+    "#SK235",
+    "Addida Shoes",
+    "Myrtie Ferguson",
+    "08-12-2020",
+    "$100",
+    "Prepaid"
+  ),
+  createData(
+    "#SK236",
+    "Sleeve Jacket",
+    "Johnny Herrera",
+    "07-30-2020",
+    "$1020",
+    "Prepaid"
+  ),
+  createData("#SK237", "Bicycle", "Ina Hughes", "08-21-2020", "$1250", "COD"),
+  createData(
+    "#SK238",
+    "Addida Shoes",
+    "Johnny Herrera",
+    "08-12-2020",
+    "$100",
+    "Prepaid"
+  ),
+  createData(
+    "#SK239",
+    "Sleeve Jacket",
+    "Myrtie Ferguson",
+    "07-30-2020",
+    "$1020",
+    "Prepaid"
+  ),
+  createData("#SK240", "Bicycle", "Ina Hughes", "08-21-2020", "$1250", "COD"),
+  createData(
+    "#SK241",
+    "Addida Shoes",
+    "Myrtie Ferguson",
+    "08-12-2020",
+    "$100",
+    "Prepaid"
+  ),
+  createData(
+    "#SK242",
+    "Sleeve Jacket",
+    "Johnny Herrera",
+    "07-30-2020",
+    "$1020",
+    "Prepaid"
+  ),
+  createData("#SK243", "Bicycle", "Ina Hughes", "08-21-2020", "$1250", "COD"),
+  createData(
+    "#SK244",
+    "Addida Shoes",
+    "Myrtie Ferguson",
+    "08-12-2020",
+    "$100",
+    "Prepaid"
+  ),
+  createData(
+    "#SK245",
+    "Sleeve Jacket",
+    "Johnny Herrera",
+    "07-30-2020",
+    "$1020",
+    "Prepaid"
+  ),
+  createData("#SK246", "Bicycle", "Ina Hughes", "08-21-2020", "$1250", "COD"),
+  createData(
+    "#SK247",
+    "Addida Shoes",
+    "Myrtie Ferguson",
+    "08-12-2020",
+    "$100",
+    "Prepaid"
+  ),
+  createData(
+    "#SK248",
+    "Sleeve Jacket",
+    "Johnny Herrera",
+    "07-30-2020",
+    "$1020",
+    "Prepaid"
+  ),
+  createData("#SK249", "Bicycle", "Ina Hughes", "08-21-2020", "$1250", "COD"),
+  createData(
+    "#SK250",
+    "Addida Shoes",
+    "Myrtie Ferguson",
+    "08-12-2020",
+    "$100",
+    "Prepaid"
+  ),
 ];
 
 function Orders() {
@@ -104,46 +227,60 @@ function Orders() {
           </Paper>
         </Box>
       </Stack>
-      <TableContainer sx={{ width: "1300px" }} component={Paper}>
-        <Table  aria-label="simple table">
-          <TableHead sx={{height:"20px"}}>
-            <TableRow style={{ backgroundColor: "#F4F7FE" }}>
-              <TableCell sx={{ fontWeight: "bold" }}>Order ID</TableCell>
-              <TableCell sx={{ fontWeight: "bold" }}>Product</TableCell>
-              <TableCell sx={{ fontWeight: "bold" }}>Customer</TableCell>
-              <TableCell sx={{ fontWeight: "bold" }}>Delivery Date</TableCell>
-              <TableCell sx={{ fontWeight: "bold" }}>Price</TableCell>
-              <TableCell sx={{ fontWeight: "bold" }}>Payment Method</TableCell>
-              <TableCell sx={{ fontWeight: "bold" }}>Status</TableCell>
-              <TableCell sx={{ fontWeight: "bold" }}>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell sx={{
-              textDecoration: 'underline',
-              color: 'blue',
-              fontSize:'15px',
-              height:'80px'
-            }} component="th" scope="row">
-                  {row.id}
+      <Scrollbars
+        style={{ width: 1300, height: 430 }}
+        autoHide={true}
+        renderThumbVertical={({ style, ...props }) => (
+          <div {...props} style={{ ...style, ...thumbStyle }} />
+        )}
+      >
+        <TableContainer sx={{ width: "1300px" }} component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead sx={{ height: "20px" }}>
+              <TableRow style={{ backgroundColor: "#F4F7FE" }}>
+                <TableCell sx={{ fontWeight: "bold" }}>Order ID</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Product</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Customer</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Delivery Date</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Price</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>
+                  Payment Method
                 </TableCell>
-                <TableCell >{row.calories}</TableCell>
-                <TableCell >{row.fat}</TableCell>
-                <TableCell >{row.carbs}</TableCell>
-                <TableCell >{row.protein}</TableCell>
-                <TableCell >{row.calories}</TableCell>
-                <TableCell >{row.fat}</TableCell>
-                <TableCell >{row.carbs}</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Status</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Actions</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell
+                    sx={{
+                      textDecoration: "underline",
+                      color: "#A0C4EB",
+                      fontSize: "15px",
+                      height: "80px",
+                    }}
+                    component="th"
+                    scope="row"
+                  >
+                    {row.id}
+                  </TableCell>
+                  <TableCell>{row.product}</TableCell>
+                  <TableCell>{row.customer}</TableCell>
+                  <TableCell>{row.delivery}</TableCell>
+                  <TableCell>{row.price}</TableCell>
+                  <TableCell>{row.payment}</TableCell>
+                  <TableCell>{row.fat}</TableCell>
+                  <TableCell>{row.carbs}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Scrollbars>
     </Box>
   );
 }
