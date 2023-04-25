ff72eda660c30d907c6b43e716712472a354cb22
harisishtiaq1 <harisharry232@gmail.com>
2023-04-25 12:23:08 +0500

    updated

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -6,17 +6,22 @@ function ProductDetails() {
   return (
     <Paper
       sx={{
-        height: "300px",
         mt: 10,
         ml: 3,
+        borderRadius: "16px",
       }}
     >
-      <Typography sx={{ fontWeight: "bold" }}>
+      <Typography sx={{ fontWeight: "bold", ml: 3 }}>
         HP Pavilion Gaming Ryzen 7 Octa Core 4800H
       </Typography>
-      <Stack direction="row">
+      <Stack direction="row" sx={{ ml: 3 }}>
         <Rating name="read-only" value={value} readOnly />
-        <Typography sx={{ ml: 2, mt: 1 }}>4356 reviews</Typography>
+        <Typography sx={{ ml: 2, color: "grey", fontSize: "13px", mt: 0.5 }}>
+          4356 reviews
+        </Typography>
+        <Typography sx={{ ml: 2, fontSize: "13px", mt: 0.5 }}>
+          SKU : MB023
+        </Typography>
       </Stack>
     </Paper>
   );
