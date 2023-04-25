4fb09cd0dec5a352e43a9568f24881694b130703
harisishtiaq1 <harisharry232@gmail.com>
2023-04-25 13:59:23 +0500

    updated

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Divider,
   IconButton,
   Paper,
@@ -8,7 +9,8 @@ import {
 } from "@mui/material";
 import React from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
 function ProductDetails() {
   return (
     <Paper
@@ -18,24 +20,66 @@ function ProductDetails() {
         borderRadius: "16px",
       }}
     >
-      <Typography sx={{ fontWeight: "bold", ml: 3 }}>
-        HP Pavilion Gaming Ryzen 7 Octa Core 4800H
-      </Typography>
-      <Stack direction="row" sx={{ ml: 3 }}>
-        <Rating name="read-only" value={4} readOnly />
-        <Typography sx={{ ml: 2, color: "grey", fontSize: "13px", mt: 0.5 }}>
-          4356 reviews
-        </Typography>
-        <Typography sx={{ ml: 2, fontSize: "13px", mt: 0.5 }}>
-          SKU : MB023
-        </Typography>
+      <Stack
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: "row",
+        }}
+      >
+        <Stack direction="column">
+          <Typography sx={{ fontWeight: "bold", ml: 3 }}>
+            HP Pavilion Gaming Ryzen 7 Octa Core 4800H
+          </Typography>
+          <Stack direction="row" sx={{ ml: 3 }}>
+            <Rating name="read-only" value={4} readOnly />
+            <Typography
+              sx={{ ml: 2, color: "grey", fontSize: "13px", mt: 0.5 }}
+            >
+              4356 reviews
+            </Typography>
+            <Typography sx={{ ml: 2, fontSize: "13px", mt: 0.5 }}>
+              SKU : MB023
+            </Typography>
+          </Stack>
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          <IconButton
+            sx={{
+              backgroundColor: "rgb(0, 127, 177)",
+              color: "white",
+              width: "30px",
+              height: "30px",
+            }}
+          >
+            <LinkedInIcon fontSize="small" />
+          </IconButton>
+          <IconButton
+            sx={{
+              backgroundColor: "rgb(59, 89, 152)",
+              color: "white",
+              width: "30px",
+              height: "30px",
+            }}
+          >
+            <FacebookIcon fontSize="small" />
+          </IconButton>
+          <IconButton
+            sx={{
+              backgroundColor: "rgb(0, 172, 237)",
+              color: "white",
+              width: "30px",
+              height: "30px",
+            }}
+          >
+            <TwitterIcon fontSize="small" />
+          </IconButton>
+        </Stack>
       </Stack>
-      <IconButton>
-        <LinkedInIcon />
-      </IconButton>
       <Divider variant="middle" sx={{ mt: 3 }}></Divider>
     </Paper>
   );
 }
-
+    
 export default ProductDetails;
