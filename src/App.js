f33a1f8c25dca6fb33ad075942d1a30e98f1b55c
harisishtiaq1 <harisharry232@gmail.com>
2023-04-26 11:59:53 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -6,28 +6,35 @@ import Checkout from "../src/Components/CheckOut/Checkout";
 import Filters from "../src/Components/Products/Filters";
 import { Box } from "@mui/material";
 import Cart from "./Components/Cart/Cart";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import ProductDetails from "./Components/ProductDetails/ProductDetails";
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Poppins, sans-serif",
+    },
+  });
   return (
     <BrowserRouter>
-      <Box
-        sx={{
-          backgroundColor: "#F4F7FE",
-          fontFamily: "Poppins, sans-serif",
-          height: "90vh",
-        }}
-      >
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Filters />} />
-          <Route path="/details" exact element={<ProductDetails />} />
-          <Route path="/orders" exact element={<Orders />} />
-          <Route path="/checkout" exact element={<Checkout />} />
-          <Route path="/cart" exact element={<Cart />} />
-          <Route path="/products" exact element={<Filters />} />
-          <Route path="/checkout" exact element={<Checkout />} />
-        </Routes>
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            backgroundColor: "#F4F7FE",
+            height: "90vh",
+          }}
+        >
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Filters />} />
+            <Route path="/details" exact element={<ProductDetails />} />
+            <Route path="/orders" exact element={<Orders />} />
+            <Route path="/checkout" exact element={<Checkout />} />
+            <Route path="/cart" exact element={<Cart />} />
+            <Route path="/products" exact element={<Filters />} />
+            <Route path="/checkout" exact element={<Checkout />} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
