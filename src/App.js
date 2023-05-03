d7ed0b0cd8a6e4ff926af0c80a4fc4dae69103ab
harisishtiaq1 <harisharry232@gmail.com>
2023-05-03 17:11:21 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Navbar from "../src/Components/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Orders from "../src/Components/RecentOrders/Orders";
 import Checkout from "../src/Components/CheckOut/Checkout";
-import Filters from "../src/Components/Products/Filters";
-import { Box } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
 import { Toaster } from "react-hot-toast";
-import Cart from "./Components/Cart/Cart";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import ProductDetails from "./Components/ProductDetails/ProductDetails";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+const Cart = React.lazy(() => import("./Components/Cart/Cart"));
+const Filters = React.lazy(() => import("./Components/Products/Filters"));
+
 function App() {
+  const [showFallback, setShowFallback] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowFallback(false);
+    }, 2000); // 30 seconds
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
   const theme = createTheme({
     typography: {
       fontFamily: "Poppins, sans-serif",
@@ -41,11 +53,136 @@ function App() {
           />
           <Navbar />
           <Routes>
-            <Route path="/" exact element={<Filters />} />
-            <Route path="/details" exact element={<ProductDetails />} />
-            <Route path="/orders" exact element={<Orders />} />
-            <Route path="/checkout" exact element={<Checkout />} />
-            <Route path="/cart" exact element={<Cart />} />
+            <Route
+              path="/"
+              exact
+              element={
+                <Suspense>
+                  {showFallback ? (
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <CircularProgress disableShrink />
+                    </Box>
+                  ) : (
+                    <Filters />
+                  )}
+                </Suspense>
+              }
+            />
+            <Route
+              path="/details"
+              exact
+              element={
+                <Suspense>
+                  {showFallback ? (
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <CircularProgress disableShrink />
+                    </Box>
+                  ) : (
+                    <ProductDetails />
+                  )}
+                </Suspense>
+              }
+            />
+            <Route
+              path="/orders"
+              exact
+              element={
+                <Suspense>
+                  {showFallback ? (
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <CircularProgress disableShrink />
+                    </Box>
+                  ) : (
+                    <Orders />
+                  )}
+                </Suspense>
+              }
+            />
+            <Route
+              path="/checkout"
+              exact
+              element={
+                <Suspense>
+                  {showFallback ? (
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <CircularProgress disableShrink />
+                    </Box>
+                  ) : (
+                    <Checkout />
+                  )}
+                </Suspense>
+              }
+            />
+            <Route
+              path="/cart"
+              exact
+              element={
+                <Suspense>
+                  {showFallback ? (
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <CircularProgress disableShrink />
+                    </Box>
+                  ) : (
+                    <Cart />
+                  )}
+                </Suspense>
+              }
+            />
             <Route path="/products" exact element={<Filters />} />
             <Route path="/checkout" exact element={<Checkout />} />
           </Routes>
